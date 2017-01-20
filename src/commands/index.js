d188ee6ba3d28d9be1767b2af3cad50df5a08904
Jed Fox <git@twopointzero.us>
2017-01-20 18:20:40 -0500

    There’s no need for a function to specify the docs.

--- src/commands/index.js
+++ src/commands/index.js
@@ -4,27 +4,27 @@ const {formatBranch: br} = interactor
 
 const annotate = (f, docs, alternates) => {
   const cmd = (...args) => Promise.resolve(f(interactor, ...args))
-  Object.defineProperty(cmd, 'docs', {get: docs})
+  Object.defineProperty(cmd, 'docs', {get: () => docs})
   cmd.alternates = alternates || []
   return cmd
 }
 
-exports['init'] = annotate(require('./init'), (DOC) => 'Initialize the repository with the necessary branches')
+exports['init'] = annotate(require('./init'), 'Initialize the repository with the necessary branches')
 
-exports['make-feature'] = annotate(require('./feature').make, (DOC) => `Branch a new feature branch off of ${br(BRANCHES.develop)}`, ['feature', 'feat', 'f'])
-exports['save-feature'] = annotate(require('./feature').save, (DOC) => `Save the current feature branch to ${br(BRANCHES.develop)}`)
+exports['make-feature'] = annotate(require('./feature').make, `Branch a new feature branch off of ${br(BRANCHES.develop)}`, ['feature', 'feat', 'f'])
+exports['save-feature'] = annotate(require('./feature').save, `Save the current feature branch to ${br(BRANCHES.develop)}`)
 
-exports['make-release'] = annotate(require('./release').make, (DOC) => `Branch a new feature branch off of ${br(BRANCHES.develop)}`, ['release', 'rel', 'r'])
-exports['save-release'] = annotate(require('./release').save, (DOC) => `Save the current release branch to ${br(BRANCHES.develop)} and ${br(BRANCHES.master)}`)
+exports['make-release'] = annotate(require('./release').make, `Branch a new feature branch off of ${br(BRANCHES.develop)}`, ['release', 'rel', 'r'])
+exports['save-release'] = annotate(require('./release').save, `Save the current release branch to ${br(BRANCHES.develop)} and ${br(BRANCHES.master)}`)
 
-exports['make-hotfix'] = annotate(require('./hotfix').make, (DOC) => `Branch a new feature branch off of ${br(BRANCHES.master)}`, ['hotfix', 'hot', 'h'])
-exports['save-hotfix'] = annotate(require('./hotfix').save, (DOC) => `Save the current hotfix branch to ${br(BRANCHES.develop)} (or the current release branch) and ${br(BRANCHES.master)}`)
+exports['make-hotfix'] = annotate(require('./hotfix').make, `Branch a new feature branch off of ${br(BRANCHES.master)}`, ['hotfix', 'hot', 'h'])
+exports['save-hotfix'] = annotate(require('./hotfix').save, `Save the current hotfix branch to ${br(BRANCHES.develop)} (or the current release branch) and ${br(BRANCHES.master)}`)
 
-exports['save'] = annotate(require('./save'), (DOC) => `Save the current feature or release branch.`, ['s'])
+exports['save'] = annotate(require('./save'), `Save the current feature or release branch.`, ['s'])
 
-exports['help'] = annotate(require('../help/cli'), (DOC) => 'View all commands', ['--help', '-h', '-?', 'h'])
+exports['help'] = annotate(require('../help/cli'), 'View all commands', ['--help', '-h', '-?', 'h'])
 
 exports['options'] = annotate(() => {
   interactor.log(require('prettify-json')(require('../conf')))
   return Promise.resolve()
-}, (DOC) => 'View the current option set', ['opts'])
+}, 'View the current option set', ['opts'])
