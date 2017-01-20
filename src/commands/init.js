740348eb439cbbdaa721a2eaa439145f12ae1791
Jed Fox <git@twopointzero.us>
2017-01-20 18:21:30 -0500

    Use `NAME` in init; reformat code

--- src/commands/init.js
+++ src/commands/init.js
@@ -4,5 +4,8 @@ const chalk = require('chalk')
 const NAME = require('../../package.json').name
 
 module.exports = ({ ok, failTo }) => {
-  return git.createOrSwitch('checkout', '-b', BRANCHES.master).then(() => git.createOrSwitch('checkout', '-b', BRANCHES.develop)).then(() => ok(`Added branches ${BRANCHES.master} and ${BRANCHES.develop}. Try ${chalk.bold(process.argv[1] + ' make-feature')} to get started.`)).catch(err => failTo(`initialize ${NAME}`, err))
+  return git.createOrSwitch('checkout', '-b', BRANCHES.master)
+            .then(() => git.createOrSwitch('checkout', '-b', BRANCHES.develop))
+            .then(() => ok(`Added branches ${BRANCHES.master} and ${BRANCHES.develop}. Try ${chalk.bold(NAME + ' make-feature')} to get started.`))
+            .catch(err => failTo(`initialize ${NAME}`, err))
 }
